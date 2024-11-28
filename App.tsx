64fff9fdc4254d2450528504b4ce1dd53083ff21
ndpniraj <ndpniraj@gmail.com>
2024-11-28 10:47:27 +0530

    sending api (get) request

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import {DefaultTheme} from '@react-navigation/native';
+import axios from 'axios';
 
 interface Props {}
 
@@ -11,8 +12,29 @@ const Theme: ReactNavigation.Theme = {
     background: '#FFFFFF',
   },
 };
-
+const url = 'http://192.168.29.217:5555/';
 const App: FC<Props> = () => {
+  useEffect(() => {
+    axios(url).then(res => {
+      console.log(res.data);
+    });
+    // fetch(url, {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    // }).then(async result => {
+    //   if (!result.ok) {
+    //     // we got error response
+    //     const errorRes = await result.json();
+    //     console.error(errorRes);
+    //   } else {
+    //     // we got success response
+    //     const res = await result.json();
+    //     console.log(res);
+    //   }
+    // });
+  }, []);
   return <AuthNavigator theme={Theme} />;
 };
 
