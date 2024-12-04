1f356b698713989a5a99a24061f2fe807a8e1599
ndpniraj <ndpniraj@gmail.com>
2024-12-04 16:11:35 +0530

    rendering category list

--- app/views/Home.tsx
+++ app/views/Home.tsx
@@ -6,6 +6,7 @@ import {
   ScrollView,
   FlatList,
   Image,
+  Pressable,
 } from 'react-native';
 import {StackScreenProps} from '@react-navigation/stack';
 import {AuthStackNavigator} from '../navigation/AuthNavigator';
@@ -17,6 +18,7 @@ type Props = StackScreenProps<AuthStackNavigator, 'Home'>;
 
 const Home: FC<Props> = ({route}) => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -29,27 +31,73 @@ const Home: FC<Props> = ({route}) => {
         console.log(error);
       }
     };
+    const fetchCategories = async () => {
+      try {
+        const {data} = await client.get<{categories: string[]}>(
+          '/product/categories/',
+        );
+        setCategories(data.categories);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
     fetchProducts();
+    fetchCategories();
   }, []);
 
   return (
-    <FlatList
-      data={products}
-      contentContainerStyle={styles.container}
-      // horizontal
-      renderItem={({item: product}) => {
-        return <ProductCard product={product} />;
-      }}
-      keyExtractor={product => product.id.toString()}
-    />
+    <View style={styles.wrapper}>
+      <FlatList
+        style={styles.categoryFlatList}
+        contentContainerStyle={styles.categoryContainer}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={categories}
+        renderItem={({item}) => {
+          return (
+            <Pressable style={styles.categoryBtn}>
+              <Text>{item}</Text>
+            </Pressable>
+          );
+        }}
+      />
+      <FlatList
+        data={products}
+        contentContainerStyle={styles.container}
+        // horizontal
+        renderItem={({item: product}) => {
+          return <ProductCard product={product} />;
+        }}
+        keyExtractor={product => product.id.toString()}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
   container: {
     paddingHorizontal: offset,
     gap: 20,
+  },
+  categoryContainer: {
+    paddingHorizontal: 10,
+    gap: 10,
+  },
+  categoryFlatList: {
+    paddingVertical: 20,
+  },
+  categoryBtn: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#dedede',
+    borderRadius: 20,
+    height: 40,
+    justifyContent: 'center',
+    alignContent: 'center',
   },
 });
 
